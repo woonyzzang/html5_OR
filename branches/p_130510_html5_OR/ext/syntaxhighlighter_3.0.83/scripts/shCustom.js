7ce9520f662e26c41a81bb2f4612357d4b035674
joeseungwoon <seungwoonjjang@gmail.com>
2013-06-11 19:20:21 +0900

    * syntaxhighlighter 초기 설정 변경

--- branches/p_130510_html5_OR/ext/syntaxhighlighter_3.0.83/scripts/shCustom.js
+++ branches/p_130510_html5_OR/ext/syntaxhighlighter_3.0.83/scripts/shCustom.js
@@ -36,5 +36,22 @@ SyntaxHighlighter.autoloader.apply(null, path(
 	'xml xhtml xslt html    @shBrushXml.js'
 ));
 
+// @SyntaxHighlighter 설정
+//SyntaxHighlighter.config.bloggerMode = true;
+//SyntaxHighlighter.config.strings.viewSource = "view my source!!!!";
+SyntaxHighlighter.config.stripBrs = true;
+//SyntaxHighlighter.config.tagName = 'pre';
+
+
+// @SyntaxHighlighter 기본값
+SyntaxHighlighter.defaults['auto-links'] = false;
+SyntaxHighlighter.defaults['class-name'] = 'code';
+//SyntaxHighlighter.defaults['collapse'] = true;
+//SyntaxHighlighter.defaults['first-line'] = 1;
+//SyntaxHighlighter.defaults['gutter'] = true;
+//SyntaxHighlighter.defaults['highlight'] = null;
+//SyntaxHighlighter.defaults['html-script'] = false;
+//SyntaxHighlighter.defaults['smart-tabs'] = true;
+//SyntaxHighlighter.defaults['tab-size'] = 4;
 SyntaxHighlighter.defaults['toolbar'] = false;
 SyntaxHighlighter.all();
