ba36d0c4cbb0a2c75cc22679a92952e4a2bdf7d3
joeseungwoon <seungwoonjjang@gmail.com>
2013-06-17 19:19:13 +0900

    * index2.html 추가 * tree > json 연동 추가

---
+++ branches/p_130510_html5_OR/index2.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<title>Web Standards Guide API</title>
+<link rel="stylesheet" href="ext/resources/css/ext-all.css">
+<link rel="stylesheet" href="ext/resources/css/base.css">
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
+<script src="ext/ext-all-debug.js"></script>
+<script src="ext/locale/ext-lang-ko.js"></script>
+</head>
+<script>
+Ext.onReady(function(){
+	var toolbarConfig = {
+		region: 'north',
+		height: 27,
+		xtype: 'toolbar',
+		items: [{
+			html: '<strong>Web Standards Guide API</strong>',
+			handler: function(e){
+				e.disable();
+			}
+		},'->',{
+			xtype: 'button',
+			text: '메뉴',
+			menu: [{
+				text: 'HTML&amp;CSS',
+				handler: function(e){
+					Ext.getCmp('convention').show();
+					Ext.getCmp('accessiblity').hide();
+					Ext.getCmp('reference').hide();
+				}
+			},{
+				text: 'Accessiblity',
+				handler: function(e){
+					Ext.getCmp('convention').hide();
+					Ext.getCmp('accessiblity').show();
+					Ext.getCmp('reference').hide();
+				}
+			},{
+				text: 'HTML5 Open Reference',
+				handler: function(e){
+					Ext.getCmp('convention').hide();
+					Ext.getCmp('accessiblity').hide();
+					Ext.getCmp('reference').show();
+				}
+			}]
+		},'-','검색', {
+			xtype : 'combo',
+			width: 80
+		},'-',{
+			xtype: 'textfield',
+			emptyText: '검색어 입력',
+			width: 150
+		}]
+	};
+
+	var convention = {
+		region: 'west',
+		title: 'HTML&amp;CSS',
+		id: 'convention',
+		collapsible: true,
+		width: 220,
+		layout: {
+			type: 'hbox',
+			align: 'stretch'
+		},
+		items: [{
+			xtype: 'treepanel',
+			flex: 1,
+			store: Ext.create('Ext.data.TreeStore', {
+				autoLoad: true,
+				proxy: {
+					type: 'ajax',
+					url: 'ext/data/tree/nodes_convention.json',
+					reader: {
+						type: 'json',
+						root: 'children'
+					}
+				}
+			}),
+			rootVisible: false
+		}]
+	};
+
+	var accessiblity = {
+		region: 'west',
+		title: 'Accessiblity',
+		id: 'accessiblity',
+		collapsible: true,
+		width: 220,
+		layout: {
+			type: 'hbox',
+			align: 'stretch'
+		},
+		items: [{
+			xtype: 'treepanel',
+			flex: 1,
+			store: Ext.create('Ext.data.TreeStore', {
+				autoLoad: true,
+				proxy: {
+					type: 'ajax',
+					url: 'ext/data/tree/nodes_accessiblity.json',
+					reader: {
+						type: 'json',
+						root: 'children'
+					}
+				}
+			}),
+			rootVisible: false
+		}]
+	};
+
+	var reference = {
+		region: 'west',
+		title: 'HTML5 Open Reference',
+		id: 'reference',
+		collapsible: true,
+		width: 220,
+		layout: {
+			type: 'hbox',
+			align: 'stretch'
+		},
+		items: [{
+			xtype: 'tabpanel',
+			flex: 1,
+			activeTab: 0,
+			items: [{
+				xtype: 'treepanel',
+				title: 'Elements',
+				store: Ext.create('Ext.data.TreeStore', {
+					autoLoad: true,
+					proxy: {
+						type: 'ajax',
+						url: 'ext/data/tree/nodes_elem.json',
+						reader: {
+							type: 'json',
+							root: 'children'
+						}
+					}
+				}),
+				rootVisible: false,
+				viewConfig: {
+					plugins: {ptype: 'treeviewdragdrop'}
+				},
+				listeners : {
+					itemclick : function($this, record, item, index, e, eOpts){
+						var path = record.data.href;
+
+						function ajaxLoad(con, idx){
+							$('#' + con).load(path[1] + ' #' + idx);
+						}
+
+						if(path){
+							Ext.get('detailDsc-innerCt').update('<iframe width=100% height=100% frameborder=0 src="' + path[0] + '"></iframe>');
+
+							for(var i=0,len=aside.items.length; i<len; i++){
+								var obj = aside.items[i],
+									idx = obj.id.replace('Area','');
+
+								ajaxLoad(obj.id + '-innerCt', idx);
+							}
+						}else{
+							return false;
+						}
+
+						ajaxLoad('support-innerCt', 'browser');
+
+						e.preventDefault();
+					}
+				}
+			},{
+				xtype: 'treepanel',
+				title: 'Attributes',
+				store: Ext.create('Ext.data.TreeStore', {
+					autoLoad: true,
+					proxy: {
+						type: 'ajax',
+						url: 'ext/data/tree/nodes_attr.json',
+						reader: {
+							type: 'json',
+							root: 'children'
+						}
+					}
+				}),
+				rootVisible: false,
+				listeners : {
+					itemclick : function($this, record, item, index, e, eOpts){
+						var path = record.data.href;
+
+						if(path){
+							Ext.get('detailDsc-innerCt').update('<iframe width=100% height=100% frameborder=0 src="' + path[0] + '"></iframe>');
+						}else{
+							return false;
+						}
+
+						e.preventDefault();
+					}
+				}
+			},{
+				title: 'Events',
+				html: '내용3',
+				border: false
+			}],
+			listeners: {
+				tabchange: function(tabPanel, newCard, oldCard, eOpts){
+					var textVal = '';
+
+					function toggleEft(options){
+						var $this = options,
+							$selecter = $this.selecter,
+							visible = $this.visible,
+							len = $selecter.length,
+							i = 0;
+
+						while(i < len){
+							if(visible == 'show'){
+								Ext.getCmp($selecter[i]).show();
+							}else{
+								Ext.getCmp($selecter[i]).hide();
+							}
+
+							i++;
+						}
+					}
+
+					switch(newCard.title){
+						case 'Elements':
+							textVal = '요소';
+
+							toggleEft({selecter:['side','support'], visible:'show'});
+							break;
+						case 'Attributes':
+							textVal = '속성';
+
+							toggleEft({selecter:['side','support'], visible:'hide'});
+							break;
+						case 'Events':
+							textVal = '이벤트';
+
+							toggleEft({selecter:['side','support'], visible:'hide'});
+							break;
+						default:break;
+					}
+
+					Ext.DomQuery.selectNode('#detailDsc_header_hd-textEl').innerHTML = textVal + ' 설명';
+				}
+			}
+		}]
+	};
+
+	var container = {
+		region: 'center',
+		title: '설명',
+		id : 'detailDsc',
+		html: '<h3>HTML5 오픈 레퍼런스 가이드 준비중...</h3>'
+	};
+
+	var aside = {
+		region: 'east',
+		title: '상세 설명',
+		id: 'side',
+		collapsible: true,
+		hidden: true,
+		width: 300,
+		layout: {
+			type: 'accordion',
+			titleCollapse: false,
+			animate: true,
+			activeOnTop: true
+		},
+		items: [{
+			title: '1. 스펙 명세',
+			id: 'referArea',
+			html: '테스트1',
+			autoHeight: true,
+			autoScroll: true
+		},{
+			title: '2. 콘텐츠 분류 타입 &frasl; 마크업 규칙',
+			id: 'flowArea',
+			html: '테스트2',
+			autoHeight: true,
+			autoScroll: true
+		},{
+			title: '3. 참조',
+			id: 'memoArea',
+			html: '테스트3',
+			autoHeight: true,
+			autoScroll: true
+		},{
+			title: '4. 관련 요소 레벨',
+			id: 'relatedArea',
+			html: '테스트4',
+			autoHeight: true,
+			autoScroll: true
+		}]
+	};
+
+	var foot = {
+		region: 'south',
+		hidden: false,
+		title: '브라우저 지원',
+		id: 'support',
+		collapsible: true,
+		collapsed: true,
+		hidden: true,
+		html: 'Support Browser',
+		height: 80,
+		minHeight: 80
+	};
+
+	var viewport = Ext.create('Ext.Viewport', {
+		layout: {
+            type: 'border',
+            padding: 3
+        },
+		defaults: {
+            split: true
+        },
+		items: [toolbarConfig, convention, accessiblity, reference, container, aside, foot]
+	});
+
+	Ext.getCmp('accessiblity').hide();
+	Ext.getCmp('reference').hide();
+});
+</script>
+</body>
+</html>
